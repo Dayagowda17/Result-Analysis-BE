0ae5a997292a4668490fc879f296c81ae99a6a40
Dayagowda17 <91661370+Dayagowda17@users.noreply.github.com>
2022-01-09 09:09:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.status(200).send('Welcome to mini project')
     console.log('welcome to vcet')
 })
-require("./route")(app);
+require("./node")(app);
 http.listen(5000);
 console.log('Server is running on port ' + ' ' + 5000, "");
 
